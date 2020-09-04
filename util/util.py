aba692326d1e627d8fbc19f1260c9a0d827c36e4
LSnyd <lisa@luethen-schneider.de>
2020-09-04 11:18:58 +0200

    perf: faster index select function

--- util/util.py
+++ util/util.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import torch
 import numpy as np
 import os
-
+import torch.nn.functional as F
 
 def mkdir(path):
     if not os.path.exists(path):
@@ -30,6 +30,33 @@ def seg_accuracy(predicted, ssegs, meshes):
         edge_areas = torch.from_numpy(mesh.get_edge_areas())
         correct += (correct_vec.float() * edge_areas).sum()
     return correct
+
+def intersection_over_union(preds, target, num_classes,  batch=1):
+        preds, target = F.one_hot(preds, num_classes), F.one_hot(target, num_classes)
+        if batch is 1:
+            i = (preds & target).sum(dim=0)
+            u = (preds | target).sum(dim=0)
+            iou = i.to(torch.float) / u.to(torch.float)
+        else:
+            iou = torch.zeros(num_classes, dtype=torch.float32)
+            for idx, pred in enumerate(preds):
+                i = (pred & target[idx]).sum(dim=0)
+                u = (pred | target[idx]).sum(dim=0)
+                iou = iou.add(i.to(torch.float) / u.to(torch.float))
+        return iou
+
+def mean_iou_calc(pred, target, num_classes, batch=None):
+    pred = torch.squeeze(pred)
+    target = torch.squeeze(target)
+    if (target == -1).any():
+        target = target[target!=-1]
+        len = target.size()
+        pred = pred[:len[0]]
+
+    iou = intersection_over_union(pred, target, num_classes, batch)
+    print(iou)
+    mean_iou = iou.mean(dim=-1)
+    return mean_iou, iou
 
 def print_network(net):
     """Print the total number of parameters in the network
@@ -66,3 +93,83 @@ def calculate_entropy(np_array):
             entropy -= a * np.log(a)
     entropy /= np.log(np_array.shape[0])
     return entropy
+
+def pad_with(vector, pad_width, iaxis, kwargs):
+    pad_value = kwargs.get('padder', 10)
+    vector[:pad_width[0]] = pad_value
+    vector[-pad_width[1]:] = pad_value
+
+#Select all rows by index
+def myindexrowselect(groups, mask_index):
+
+    index = groups._indices()
+    newrowindex = -1
+
+    print(groups, mask_index)
+
+    for ind in mask_index:
+        try:
+            newrowindex = newrowindex + 1
+        except NameError:
+            newrowindex = 0
+
+        keptindex = torch.squeeze((index[0] == ind).nonzero())
+
+        if len(keptindex.size()) == 0:
+            # Get column values from mask, create new row idx
+            try:
+                newidx = torch.cat((newidx, torch.tensor([newrowindex])), 0)
+                newcolval = torch.cat((newcolval, torch.tensor([index[1][keptindex.item()]])), 0)
+            except NameError:
+                newidx = torch.tensor([newrowindex])
+                newcolval = torch.tensor([index[1][keptindex.item()]])
+
+        else:
+            # Get column values from mask, create new row idx
+            # Add newrowindex eee.size() time to list
+            for i in range(list(keptindex.size())[0]):
+                try:
+                    newidx = torch.cat((newidx, torch.tensor([newrowindex])), 0)
+                    newcolval = torch.cat((newcolval, torch.tensor([index[1][keptindex.tolist()[i]]])), 0)
+                except NameError:
+                    newidx = torch.tensor([newrowindex])
+                    newcolval = torch.tensor([index[1][keptindex.tolist()[i]]])
+
+    groups = torch.sparse_coo_tensor(indices=torch.stack((newidx, newcolval), dim=0),
+                                     values=torch.ones(newidx.shape[0], dtype=torch.float),
+                                     size=(len(mask_index), groups.shape[1]))
+    return groups
+
+
+
+def getsparserow(groups, ind):
+
+#ind gibt index von row an
+        index = groups._indices()
+        keptindex = torch.squeeze((index[0] == ind).nonzero())
+
+
+        if len(keptindex.size()) == 0:
+            # Get column values from mask, create new row idx
+            try:
+                newidx = torch.tensor([0])
+                newcolval = torch.cat((newcolval, torch.tensor([index[1][keptindex.item()]])), 0)
+            except NameError:
+                newcolval = torch.tensor([index[1][keptindex.item()]])
+        else:
+            # Get column values from mask, create new row idx
+            # Add newrowindex eee.size() time to list
+            for i in range(list(keptindex.size())[0]):
+                try:
+                    newidx = torch.cat((newidx,  torch.tensor([0])), 0)
+                    newcolval = torch.cat((newcolval, torch.tensor([index[1][keptindex.tolist()[i]]])), 0)
+                except NameError:
+                    newidx = torch.tensor([0])
+                    newcolval = torch.tensor([index[1][keptindex.tolist()[i]]])
+
+
+
+        groups = torch.sparse_coo_tensor(indices=torch.stack((newidx, newcolval), dim=0),
+                                     values=torch.ones(newcolval.shape[0], dtype=torch.float),
+                                     size=(len(newcolval), groups.shape[1]))
+        return groups
