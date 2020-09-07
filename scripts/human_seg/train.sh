3fcb53b1aec309d4dfa41668fc626f37e6daf72a
LSnyd <lisa@luethen-schneider.de>
2020-09-07 15:21:18 +0200

    feat: add new training script

--- scripts/human_seg/train.sh
+++ scripts/human_seg/train.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
 ## run the training
-python train.py \
+python3 train.py \
 --dataroot datasets/human_seg \
 --name human_seg \
 --arch meshunet \
 --dataset_mode segmentation \
 --ncf 32 64 128 256 \
---ninput_edges 2280 \
---pool_res 1800 1350 600 \
---resblocks 3 \
---batch_size 12 \
+--ninput_edges 3000 \
+--pool_res 2000 1000 500 \
+--num_threads 0 \
+--resblocks 1 \
+--batch_size 2 \
 --lr 0.001 \
 --num_aug 20 \
 --slide_verts 0.2 \
+--gpu_ids -1 \
+--verbose_plot \
+--weighted_loss 0.125 0.125 0.125 0.125 0.125 0.125 0.125 0.125 \
