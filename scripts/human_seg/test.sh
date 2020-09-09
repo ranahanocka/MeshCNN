66e5ca1d0ef29cb00bef55830f920d8d0871b3e1
LSnyd <lisa@luethen-schneider.de>
2020-09-09 14:40:35 +0200

    fix: fit test to training

--- scripts/human_seg/test.sh
+++ scripts/human_seg/test.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 ## run the test and export collapses
-python test.py \
+python3 test.py \
 --dataroot datasets/human_seg \
 --name human_seg \
 --arch meshunet \
 --dataset_mode segmentation \
 --ncf 32 64 128 256 \
---ninput_edges 2280 \
---pool_res 1800 1350 600 \
---resblocks 3 \
+--ninput_edges 3000 \
+--pool_res 2000 1000 500 \
+--num_threads 0 \
+--resblocks 1 \
 --batch_size 12 \
 --export_folder meshes \
+--gpu_ids -1 \
