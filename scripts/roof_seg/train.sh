cb9fc57c0dbc8873601491d74b0e36d6d2e71a4f
ihahanov <ihahanov@sciforce.solutions>
2021-12-14 15:35:06 +0200

    add model 9-11 to dataset

--- scripts/roof_seg/train.sh
+++ scripts/roof_seg/train.sh
@@ -9,9 +9,11 @@ python train_pl.py \
 --ncf 32 64 128 256 \
 --ninput_edges 16000 \
 --pool_res 12000 10500 9000 \
---resblocks 2 \
+--resblocks 3 \
 --batch_size 1 \
 --lr 0.005 \
 --num_aug 20 \
---slide_verts 0.2
+--slide_verts 0.2 \
+--warmup_epochs 300
+
 #--from_pretrained checkpoints/coseg_aliens/latest_net.pth
